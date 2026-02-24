9be1c842b39adfd61dbc717ed6745cf516ae6640
이백행 <leebh@itcen.com>
2026-02-24 13:56:09 +0900

    spring-boot-starter-tomcat, spring-boot-starter-jetty, spring-boot-starter-undertow

---
+++ demo3511/src/main/java/com/example/demo3511/Demo3511Application.java
@@ -0,0 +1,13 @@
+package com.example.demo3511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3511Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3511Application.class, args);
+	}
+
+}
