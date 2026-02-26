218cf430f10c451cac78329a741408f81b48ad25
이백행 <leebh@itcen.com>
2026-02-26 15:23:42 +0900

    CRUD 추가

--- demo/src/main/java/com/example/demo/a/a/a/service/impl/AaaMapper.java
+++ demo/src/main/java/com/example/demo/a/a/a/service/impl/AaaMapper.java
@@ -13,11 +13,67 @@ import com.example.demo.a.a.a.service.AaaVo;
 public interface AaaMapper {
 
 	/**
+	 * 에이 에이 에이 등록
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	int insertAaa(AaaVo aaaVo);
+
+	/**
+	 * 에이 에이 에이 등록2
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	int insert2Aaa(AaaVo aaaVo);
+
+	/**
+	 * 에이 에이 에이 조회(단건)
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	AaaVo selectAaa(AaaVo aaaVo);
+
+	/**
 	 * 에이 에이 에이 조회(멀티건)
 	 * 
 	 * @param aaaVo
 	 * @return
 	 */
-	public List<AaaVo> selectAaaList(AaaVo aaaVo);
+	List<AaaVo> selectAaaList(AaaVo aaaVo);
+
+	/**
+	 * 에이 에이 에이 수정
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	int updateAaa(AaaVo aaaVo);
+
+	/**
+	 * 에이 에이 에이 삭제
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	int deleteAaa(AaaVo aaaVo);
+
+	/**
+	 * 에이 에이 에이 등록/수정
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	int mergeAaa(AaaVo aaaVo);
+
+	/**
+	 * 에이 에이 에이 등록/수정/삭제
+	 * 
+	 * @param aaaVo
+	 * @return
+	 */
+	int multiAaa(AaaVo aaaVo);
 
 }
